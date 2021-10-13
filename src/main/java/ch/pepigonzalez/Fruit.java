88a161fdb9f8108bff1b5e0e1db4affe90b78f2c
José-Antonio Gonzalez <gonzapep@gmail.com>
2021-10-13 11:02:53 +0200

    Test One

---
+++ src/main/java/ch/pepigonzalez/Fruit.java
@@ -0,0 +1,30 @@
+package ch.pepigonzalez;
+
+public class Fruit {
+    public String name;
+    public String description;
+
+    public Fruit() {
+    }
+
+    public Fruit(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/ch/pepigonzalez/FruitResource.java
@@ -0,0 +1,36 @@
+package ch.pepigonzalez;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/fruits")
+public class FruitResource {
+
+    private Set<Fruit> fruits = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
+
+    public FruitResource() {
+        fruits.add(new Fruit("Apple", "Winter fruit"));
+        fruits.add(new Fruit("Pineapple", "Tropical fruit"));
+    }
+
+    @GET
+    public Set<Fruit> list() {
+        return fruits;
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Set<Fruit> add(Fruit fruit) {
+        fruits.add(fruit);
+        return fruits;
+    }
+
+    @DELETE
+    public Set<Fruit> delete(Fruit fruit) {
+        fruits.removeIf(existingFruit -> existingFruit.name.contentEquals(fruit.name));
+        return fruits;
+    }
+}
